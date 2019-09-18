a7d47a35cd149fd97bebc779f3e76a6804bfd4a4
Abhinash Bastola <abastola@urgent.ly>
2019-09-18 18:46:28 -0400

    get call added

--- src/main/java/com/amazon/music/controllers/ArtistController.java
+++ src/main/java/com/amazon/music/controllers/ArtistController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.amazon.music.dtos.ArtistDto;
@@ -19,6 +20,13 @@ public class ArtistController {
 	@RequestMapping(value = "", method = RequestMethod.POST)
 	public void createArtist(@RequestBody ArtistDto artistDto) {
 		artistService.createArtist(artistDto);
+	}
+	
+	@RequestMapping(value="", method = RequestMethod.GET)
+	public ArtistDto getArtistById(@RequestParam(value="id", required = true) Long artistId,
+			@RequestParam(value = "name", required = false) String name) {
+		System.out.println("id : " + artistId + " , name : " + name);
+		return artistService.getArtistById(artistId);
 	}
 
 }
--- src/main/java/com/amazon/music/services/ArtistService.java
+++ src/main/java/com/amazon/music/services/ArtistService.java
@@ -10,4 +10,13 @@ public class ArtistService {
 	public void createArtist(ArtistDto artistDto) {
 		System.out.println(artistDto.toString());
 	}
+	
+	public ArtistDto getArtistById(Long id) {
+		ArtistDto artist = new ArtistDto();
+		artist.setName("Ed Sheeran");
+		artist.setGenre("pop");
+		artist.setCountry("ENG");
+		
+		return artist;
+	}
 }
