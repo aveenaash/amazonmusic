807af0a184c24bc754bf9604d3a8a7f46e9c37c0
Abhinash Bastola <abastola@urgent.ly>
2019-09-19 14:20:06 -0400

    pathvariable added

--- src/main/java/com/amazon/music/controllers/ArtistController.java
+++ src/main/java/com/amazon/music/controllers/ArtistController.java
@@ -1,7 +1,9 @@
 package com.amazon.music.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,6 +28,14 @@ public class ArtistController {
 	public ArtistDto getArtistById(@RequestParam(value="id", required = true) Long artistId,
 			@RequestParam(value = "name", required = false) String name) {
 		System.out.println("id : " + artistId + " , name : " + name);
+		return artistService.getArtistById(artistId);
+	}
+	
+	// http://localhost:8000/amazonmusic/artist/20/detail
+	@RequestMapping(value ="/{id}/detail", method = RequestMethod.GET)
+	public ArtistDto getArtistFromId(@PathVariable(value="id") Long artistId,
+			@RequestHeader(value="auth-token") String authToken) {
+		System.out.println("auth token : " + authToken);
 		return artistService.getArtistById(artistId);
 	}
 
